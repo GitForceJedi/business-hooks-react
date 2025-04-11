bd6568fe4d6e9bcb5d50a343a45c9c86a9f4e0e3
Ken C <kenwillcode@outlook.com>
2025-04-11 16:30:01 -0400

    finishing touches on design and labeling

--- src/App.js
+++ src/App.js
@@ -28,14 +28,33 @@ function App() {
         className="left-panel"
         style={{ padding: '2rem', fontFamily: 'Arial' }}
       >
-        <h1>Custom React Hooks - Business</h1>
+        <h1>Custom React Hooks - Business Logic</h1>
         <p>
           This project is meant to showcase the power of custom React Hooks, in
           the scope of Business Profit and Margin. Try adjusting both the
+          Purchase Price (which influences the discount applied) and the
+          Business Cost (which influences the profit margin). At the bottom of
+          the left column, the profit amount is categorized into Low, Moderate,
+          and High profit margins
+          <p>
+            Finally, the two graphs within the right column will adjust to
+            reflect the amounts returned by the custom React Hooks and adjust
+            their displays accordingly. This showcases the power of custom React
+            Hooks working in conjunction with one another. As well as, how the
+            results can be harnessed in additional elements of the website.
+          </p>
         </p>
 
-        <h2>useDiscountCalculator</h2>
-        <h3>Customer Purchase Amount:</h3>
+        <h2 class="main-color">useDiscountCalculator Hook</h2>
+        <h3>Original Purchase Price:</h3>
+        <p>
+          &#40;
+          <i>
+            Adjust the amount below. Higher prices will trigger higher
+            discounts.
+          </i>
+          &#41;
+        </p>
         <input
           type="text"
           inputMode="decimal"
@@ -48,8 +67,16 @@ function App() {
         <p>Discount: {discountData.discountRate * 100}%</p>
         <p>Final: ${discountData.discountedAmount}</p>
 
-        <h2>useProfitCalculator</h2>
-        <h3>Business Cost Amount:</h3>
+        <h2 class="main-color">useProfitCalculator Hook</h2>
+        <h3>Business Cost:</h3>
+        <p>
+          &#40;
+          <i>
+            Adjust the amount below. Profit margin and category will be
+            affected.
+          </i>
+          &#41;
+        </p>
         <input
           type="text"
           inputMode="decimal"
@@ -62,33 +89,41 @@ function App() {
         <p>Sale Price (after discount): ${discountData.discountedAmount}</p>
         <p>Profit: ${profitData.profit}</p>
         <p>Margin: {profitData.marginPercent}%</p>
-        <h2>useProfitCategory</h2>
-        <p>Category: {profitCategory}</p>
+        <h2 class="main-color">useProfitCategory Hook</h2>
+        <h3>
+          Profit Margin: <b class="main-color">{profitCategory} Margin</b>
+        </h3>
       </div>
 
       <div className="right-panel">
         <div className="chart-container">
           <div className="chart-box">
-            <h3>Bar Chart</h3>
+            <h3>Monetary Values</h3>
+            <br />
+            <br></br>
+
             <BarChart
-              width={500}
+              width={700}
               height={200}
               data={[
-                { name: 'Original', value: discountData.originalAmount },
-                { name: 'Discounted', value: discountData.discountedAmount },
-                { name: 'Profit', value: Number(profitData.profit) },
+                { name: 'Original Price', value: discountData.originalAmount },
+                {
+                  name: 'Discounted (Paid Price)',
+                  value: discountData.discountedAmount,
+                },
+                { name: 'Profit Amount', value: Number(profitData.profit) },
               ]}
             >
               <XAxis dataKey="name" />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="value" fill="#8884d8" />
+              <Bar dataKey="value" fill="#FF4500" />
             </BarChart>
           </div>
           <div className="chart-box">
-            <h3>Pie Chart</h3>
-            <PieChart width={500} height={200}>
+            <h3>Cost vs. Profit</h3>
+            <PieChart width={700} height={300}>
               <Pie
                 dataKey="value"
                 isAnimationActive={false}
@@ -98,12 +133,12 @@ function App() {
                 ]}
                 cx="50%"
                 cy="50%"
-                outerRadius={60}
-                fill="#82ca9d"
+                outerRadius={100}
+                fill="#FF4500"
                 label
               >
-                <Cell fill="#8884d8" />
-                <Cell fill="#82ca9d" />
+                <Cell fill="#333333" />
+                <Cell fill="#FF4500" />
               </Pie>
               <Tooltip />
               <Legend />
